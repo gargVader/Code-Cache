953d7c9d9c69e37886b16d6d6fe28e751b4dcfbf
gargVader <girishgargcool@gmail.com>
2021-07-19 15:55:13 +0530

    admin app added

---
+++ admin/src/main/java/com/example/admin/AddEventFragment.java
@@ -0,0 +1,19 @@
+package com.example.admin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddEventFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_event, container, false);
+    }
+}
---
+++ admin/src/main/java/com/example/admin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setSupportActionBar(toolbar);
+        NavigationUI.setupWithNavController(toolbar, navController);
+    }
+}
