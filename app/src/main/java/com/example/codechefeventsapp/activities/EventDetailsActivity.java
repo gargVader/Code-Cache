8d050fa37a8b12e1bd65d554f84c78413da4020c
gargVader <girishgargcool@gmail.com>
2021-07-26 00:28:27 +0530

    EventDetailsActivity UI partially done

--- app/src/main/java/com/example/codechefeventsapp/activities/EventDetailsActivity.java
+++ app/src/main/java/com/example/codechefeventsapp/activities/EventDetailsActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.transition.TransitionInflater;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.example.codechefeventsapp.R;
+import static com.example.codechefeventsapp.activities.MainActivity.TAG;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class EventDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
 
-        getWindow().setSharedElementEnterTransition(TransitionInflater.from(this).inflateTransition(android.R.transition.explode));
+        Log.d(TAG, "onCreate: "+this.getClass().getSimpleName());
+
     }
 }
