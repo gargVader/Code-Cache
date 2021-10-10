a1df163faf2734efb032112fc2cce341b4825e2a
gargVader <girishgargcool@gmail.com>
2021-10-10 19:00:24 +0530

    Shake to register added

--- app/src/main/java/com/example/codechefeventsapp/activities/EventDetailsActivity.java
+++ app/src/main/java/com/example/codechefeventsapp/activities/EventDetailsActivity.java
@@ -1,6 +1,11 @@
 package com.example.codechefeventsapp.activities;
 
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,8 +17,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.codechefeventsapp.R;
 import com.example.codechefeventsapp.data.Utils;
 import com.example.codechefeventsapp.data.models.Event;
+import com.squareup.seismic.ShakeDetector;
 
-public class EventDetailsActivity extends AppCompatActivity {
+public class EventDetailsActivity extends AppCompatActivity implements ShakeDetector.Listener {
 
     private Button registerB;
     Event event;
@@ -44,6 +50,8 @@ public class EventDetailsActivity extends AppCompatActivity {
                 registerUserForEvent();
             }
         });
+
+        initSensor();
     }
 
     void registerUserForEvent() {
@@ -73,6 +81,26 @@ public class EventDetailsActivity extends AppCompatActivity {
 
     boolean isUseable(String string) {
         return string != null && !(string.trim().isEmpty());
+    }
+
+    @Override
+    public void hearShake() {
+        vibratePhone();
+    }
+
+    void initSensor() {
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        ShakeDetector sd = new ShakeDetector(this);
+        sd.setSensitivity(10);
+        sd.start(sensorManager);
+    }
+
+    void vibratePhone() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            v.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+
     }
 
 }
